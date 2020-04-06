128a1e958b53ee0155ec3ed695dc2de524d4c009
Matthew Cheney <matt1cheney@gmail.com>
2020-04-06 19:03:47 -0400

    auth0 misc. files

--- exec.sh
+++ exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t auth0-javascript-sample-01-login .
-docker run --init -p 3000:3000 -it auth0-javascript-sample-01-login
+docker run --init -p 8080:8080 -it auth0-javascript-sample-01-login
