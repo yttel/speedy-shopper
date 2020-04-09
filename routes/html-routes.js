7d994bccc70f02357499153f32d25d984c42acf5
A Hoerman <awhoerman@me.com>
2020-04-09 12:43:08 -0400

    html routes and goshop

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const groceryCont = require("../controller/groceryController")
 
-router.get("/*", function(req, res){
-  res.render("index.handlebars");
-});
 
 router.get("/", (req, res) => {
   res.send(req.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+router.get("/list", function(req, res){
+  groceryCont.allByHousehold(1).then(response => {
+    var hbsObject = {
+      listItem: response
+    };
+    res.render("list.handlebars", hbsObject);
+  });
 });
 
 module.exports = router;
