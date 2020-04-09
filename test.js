dd87b0d2543449dbe526a59bcc9de7ba649eefcc
BytteL <lettybedard@gmail.com>
2020-04-08 21:41:18 -0400

    console table

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ const db = require("./models");
 //   groceryController.itemsByCategory(1)
 //     .then((things) => { 
 //       const jsonThings = things.map((thing) => thing.toJSON());
-//       console.log(jsonThings);
+//       console.table(jsonThings);
 //       return Promise.resolve();
 //     }).then(() => db.sequelize.close());
 // });
@@ -13,7 +13,7 @@ const db = require("./models");
 // db.sequelize.sync({ force: false }).then(function() {
 //   groceryController.itemsByCategory(1)
 //     .then((things) => { 
-//       console.log(things);
+//       console.table(things);
 //       return Promise.resolve();
 //     }).then(() => db.sequelize.close());
 // });
@@ -21,23 +21,23 @@ const db = require("./models");
 // db.sequelize.sync({ force: false }).then(function() {
 //   groceryController.allByHousehold(2)
 //     .then((things) => { 
-//       console.log(things);
+//       console.table(things);
 //       return Promise.resolve();
 //     }).then(() => db.sequelize.close());
 // });
 
-// db.sequelize.sync({ force: false }).then(function() {
-//   groceryController.allCategory()
-//     .then((things) => { 
-//       console.log(things);
-//       return Promise.resolve();
-//     }).then(() => db.sequelize.close());
-// });
+db.sequelize.sync({ force: false }).then(function() {
+  groceryController.allCategory()
+    .then((things) => { 
+      console.table(things);
+      return Promise.resolve();
+    }).then(() => db.sequelize.close());
+});
 
 // db.sequelize.sync({ force: false }).then(function() {
 //   groceryController.addCategory("sweets")
 //     .then((things) => { 
-//       console.log(things);
+//       console.table(things);
 //       return Promise.resolve();
 //     }).then(() => db.sequelize.close());
 // });
@@ -45,15 +45,15 @@ const db = require("./models");
 // db.sequelize.sync({ force: false }).then(function() {
 //   groceryController.gotIt(9)
 //     .then((things) => { 
-//       console.log(things);
+//       console.table(things);
 //       return Promise.resolve();
 //     }).then(() => db.sequelize.close());
 // });
 
-db.sequelize.sync({ force: false }).then(function() {
-  groceryController.nextTime(5)
-    .then((things) => { 
-      console.log(things);
-      return Promise.resolve();
-    }).then(() => db.sequelize.close());
-});
+// db.sequelize.sync({ force: false }).then(function() {
+//   groceryController.nextTime(5)
+//     .then((things) => { 
+//       console.table(things);
+//       return Promise.resolve();
+//     }).then(() => db.sequelize.close());
+// });
