9110157c40c68e74cd040139e66b79cc5f585fe0
A Hoerman <awhoerman@me.com>
2020-04-11 09:38:34 -0400

    html route changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,17 +2,17 @@ const express = require("express");
 const router = express.Router();
 const groceryCont = require("../controller/groceryController");
 
-router.get("/", function (req, res) {
-  res.render("index.handlebars");
-});
+// router.get("/", function (req, res) {
+//   res.render("index.handlebars");
+// });
 
 router.get("/signin", function (req, res) {
   res.render("signin.handlebars");
 });
 
-router.get("/", (req, res) => {
-  res.send(req.isAuthenticated() ? "Logged in" : "Logged out");
-});
+// router.get("/", (req, res) => {
+//   res.send(req.isAuthenticated() ? "Logged in" : "Logged out");
+// });
 
 router.get("/list", function (req, res) {
   const hhID = 1; //dynamic later
@@ -22,6 +22,15 @@ router.get("/list", function (req, res) {
     };
     console.log(response);
     res.render("list.handlebars", hbsObject);
+  });
+});
+
+router.get("/", function (req, res) {
+  groceryCont.allItems().then((response) => {
+    var hbsObject = {
+      Item: response,
+    };
+    res.render("index.handlebars", hbsObject);
   });
 });
 
