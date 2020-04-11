cd6c30ee20f2d946cfa6e6dd9d73c2c531102c75
Danielle Daley <danielledaley2196@gmail.com>
2020-04-11 01:55:11 -0400

    edited edit route

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const groceryCont = require("../controller/groceryController");
 
-router.get("/", function(req, res){
+router.get("/", function (req, res) {
   res.render("index.handlebars");
 });
 
-router.get("/signin", function(req, res){
+router.get("/signin", function (req, res) {
   res.render("signin.handlebars");
 });
 
@@ -14,21 +14,21 @@ router.get("/", (req, res) => {
   res.send(req.isAuthenticated() ? "Logged in" : "Logged out");
 });
 
-router.get("/list", function(req, res){
+router.get("/list", function (req, res) {
   const hhID = 1; //dynamic later
-  groceryCont.allByHousehold(hhID).then(response => {
+  groceryCont.allByHousehold(hhID).then((response) => {
     var hbsObject = {
-      listItem: response
+      listItem: response,
     };
     console.log(response);
     res.render("list.handlebars", hbsObject);
   });
 });
 
-router.get("/edit", function(req, res){
-  groceryCont.allItems().then(response => {
+router.get("/edit", function (req, res) {
+  groceryCont.allCategory().then((response) => {
     var hbsObject = {
-      item: response
+      Category: response,
     };
     res.render("index.handlebars", hbsObject);
   });
